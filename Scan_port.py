222690cc2c349add619fa10531b53c6e145c95b8
Mohamed Abdel Rahman Mohamed Abdel Rahman <142687594+10thoframadan@users.noreply.github.com>
2023-11-28 16:57:40 +0200

    Scan_port.py

---
+++ Scan_port.py
@@ -0,0 +1,20 @@
+import socket
+
+def def scan_port(host, port):
+  try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+    result = s.connect_ex((host, port))
+    if result == 0 :
+      print(f"Port {port} is open")
+    s.close()
+  except Exception as exception:
+    print("Exception: " + str(exception));
+
+def main():
+  host = input("Enter the Host address: ")
+  for port in range(1, 1001):
+    scan_port(host, port)
+
+if __name__ == "__main__" :
+  main()
