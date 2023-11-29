c562d210d636018fcf9ecd2459fe334e92e6ca7f
Mohamed Abdel Rahman Mohamed Abdel Rahman <142687594+10thoframadan@users.noreply.github.com>
2023-11-29 13:53:39 +0200

    Update robo-speaker.py

--- robo-speaker.py
+++ robo-speaker.py
@@ -1,14 +1,13 @@
 import win32com.client as win32com
 
 speak = win32com.Dispatch("SAPI.SpVoice")
-print("\n---- Robot Speaker ----")
-print("Welcome to robo speaker")
+print("\n---- Welcome to robo speaker ----")
 
 while True:
-  user = input("What you want to speak (type \"00\" to exit)\n: ")
-  if user == "00" :
+  admin = input("What you want to speak (type \"00\" to exit)\n: ")
+  if admin == "00":
     speak.Speak("Goodbye.")
     print("Thanks for using robo speaker")
     break
-else:
-  command = speak.Speak(user)
+  else:
+    command = speak.Speak(admin)
