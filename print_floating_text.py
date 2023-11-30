e4a353b7e92768c8485545bd2a946822b1952819
Mohamed Abdel Rahman Mohamed Abdel Rahman <142687594+10thoframadan@users.noreply.github.com>
2023-11-30 08:41:10 +0200

    Update print_floating_text.py

--- print_floating_text.py
+++ print_floating_text.py
@@ -1,17 +1,13 @@
 import time
 
-def print_floating_text(text):
+def mohamed(text):
   for char in text:
     print(char, end='', flush=True)
     time.sleep(0.05)
   print()
 
-print_floating_text("Hi, I am a Mohamed Abdel Rahman")
+mohamed("Hello world")
 time.sleep(1)
-print_floating_text("I am a computer sience")
+mohamed("Goodbye world")
 time.sleep(1)
-print_floating_text("I know python programming")
-time.sleep(1)
-print_floating_text("Learning C C++ C#")
-time.sleep(1)
-print("Successfully printed floating text effect...")
+print("Successfully...")
