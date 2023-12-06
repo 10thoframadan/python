aeedef7a1f1d46c8a4311ad70cd9ea5920d9351b
Mohamed Abdel Rahman Mohamed Abdel Rahman <142687594+10thoframadan@users.noreply.github.com>
2023-12-07 01:01:30 +0200

    Update

---
+++ OpenAIChat.py
@@ -0,0 +1,22 @@
+import openai
+                                                  def ask_openai(prompt, api_key):
+    openai.api_key = api_key                          response = openai.Completion.create(
+            engine = "text-davinci-003",
+            prompt = prompt,
+            max_tokens = 1000,                        )
+
+    return response.choices[0].text.strip()
+
+def main():
+    api_key = "YOUR_API_KEY_HERE"
+    while True:
+        user_input = input('You: ')
+        if user_input.lower() == 'exit':
+            print('Goodbye.')
+            break
+        prompt = f"You: {user_input}\nOpenAI:"
+        response = ask_openai(prompt, api_key)
+        print(response)
+
+if __name__ == "__main__":
+    main()
