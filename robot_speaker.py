138260ec7100971f0026792f5b4577674347ac60
Mohamed Abdel Rahman Mohamed Abdel Rahman <142687594+10thoframadan@users.noreply.github.com>
2023-12-15 13:23:29 +0200

    update robot_speaker.py

--- robot_speaker.py
+++ robot_speaker.py
@@ -1,11 +1,9 @@
 import win32com.client as win32com
-
 speak = win32com.Dispatch("SAPI.SpVoice")
-
 while True:
     user_input = input("What you want to speak (type \"00\" to exit)\n: ")
     if user_input == "00":
-        speak.Speak("Disconnected")
+        speak.Speak("Disconnected.")
         break
     else:
         command = speak.Speak(user_input)
