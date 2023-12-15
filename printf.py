b916827a5d950177a18edca25b4da29918afa805
Mohamed Abdel Rahman Mohamed Abdel Rahman <142687594+10thoframadan@users.noreply.github.com>
2023-12-15 13:39:56 +0200

    printf.py

---
+++ printf.py
@@ -0,0 +1,15 @@
+import time
+def printf(text):
+  for char in text:
+    print(char, end='', flush=True)
+    time.sleep(0.05)
+  print()
+printf("Hi, I am a Mohamed Abdel Rahman")
+time.sleep(1)
+printf("I am a computer sience student")
+time.sleep(1)
+printf("I am a python programmer")
+time.sleep(1)
+printf("I looking for learn c++ soon")
+time sleep(1)
+print("This my printf text effect...")
