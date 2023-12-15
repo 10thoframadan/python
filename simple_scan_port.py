1d730c68fca241f530f613bec83aa5f4b7f7ac98
Mohamed Abdel Rahman Mohamed Abdel Rahman <142687594+10thoframadan@users.noreply.github.com>
2023-12-15 13:48:35 +0200

    simple_scan_port.py

---
+++ simple_scan_port.py
@@ -0,0 +1,19 @@
+import socket
+def scan_port(host, port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(2)
+        result = s.connect_ex((host, port))
+        if result == 0:
+            print(f"Port {port} is open")
+        else:
+            print(f"Port {port} is close", end="\r")
+        s.close()
+    except Exception as e:
+        print(e)
+def main():
+    host = input("Enter the host: ")
+    for port in range(1, 1025):
+        scan_port(host, port)
+if __name__ == "__main__":
+    main()
