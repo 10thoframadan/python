8cc4878191c3b39c4be861decc14352bf741c0aa
Mohamed Abdel Rahman Mohamed Abdel Rahman <142687594+10thoframadan@users.noreply.github.com>
2023-12-19 13:39:36 +0200

    generate_hash.py

--- generate_hash.py
+++ generate_hash.py
@@ -1,11 +1,11 @@
 import hashlib
 
 def generate_hash(data, algorithm):
-  hasher = hashlib.new(algorithm)
-  data_bytes = data.encode('utf-8')
-  hasher.update(data_bytes)
-  hash_digest = hasher.hexdigest()
-  return hash_digest
+    hasher = hashlib.new(algorithm)
+    data_bytes = data.encode('utf-8')
+    hasher.update(data_bytes)
+    hash_digest = hasher.hexdigest()
+    return hash_digest
 
 data = input("Enter the data to hash: ")
 
@@ -17,15 +17,15 @@ print("4. SHA-512")
 choice = input("Enter your choice (1-4): ")
 
 algorithm_map = {
-  "1": "md5",
-  "2": "sha1",
-  "3": "sha256",
-  "4": "sha512"
+    "1": "md5",
+    "2": "sha1",
+    "3": "sha256",
+    "4": "sha512"
 }
 
 algorithm = algorithm_map.get(choice)
 if algorithm:
-  hash_result = generate_hash(data, algorithm)
-  print("Hash:", hash_result)
+    hash_result = generate_hash(data, algorithm)
+    print("Hash:", hash_result)
 else:
-  print("Invalid choice.")
+    print("Invalid choice.")
